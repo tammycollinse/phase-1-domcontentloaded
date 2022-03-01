7302adb20b494ae658088c22ef9810e6178d3fc5
Tammy <tammycollinse@gmail.com>
2022-02-28 21:20:34 -0500

    commit tree 270b4577d1a0c78967de2b3276907ab9c3ca482f

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(e) {
+   let thing =  document.querySelector("#text");
+   console.log(thing)
+   thing.textContent = "This is really cool!"
+   console.log(thing);
+  });
